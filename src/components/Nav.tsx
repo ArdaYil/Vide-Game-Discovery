9b2ddb6a159680034517f6106a292893a12dbade
= <ardayildirim05@icloud.com>
2023-03-28 17:55:20 +0200

    a

---
+++ src/components/Nav.tsx
@@ -0,0 +1,9 @@
+import { HStack, Image } from "@chakra-ui/react";
+
+export default function Nav() {
+  return (
+    <HStack>
+      <Image width="16" src="../../public/images/logo.webp" />
+    </HStack>
+  );
+}
