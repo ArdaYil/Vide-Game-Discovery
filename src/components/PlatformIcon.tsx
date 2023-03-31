e54cdace9d868435c776acb4ac44b52ad546ecfe
= <ardayildirim05@icloud.com>
2023-03-31 15:42:17 +0200

    a

---
+++ src/components/PlatformIcon.tsx
@@ -0,0 +1,22 @@
+import {
+  BsWindows,
+  BsXbox,
+  BsNintendoSwitch,
+  BsFillPhoneFill,
+} from "react-icons/bs";
+import { FaPlaystation } from "react-icons/fa";
+import { DiLinux } from "react-icons/di";
+import { AiFillApple, AiFillAndroid } from "react-icons/ai";
+
+const platformImages = {
+  1: BsWindows,
+  2: FaPlaystation,
+  3: BsXbox,
+
+  5: AiFillApple,
+  6: DiLinux,
+  7: BsNintendoSwitch,
+  8: AiFillAndroid,
+};
+
+export default function PlatformIcon() {}
